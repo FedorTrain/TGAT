d9f424b2273ab1e8ffd91a0922193931f938ef92
FedorTrain <fedya_dryagin@mail.ru>
2022-01-18 15:37:44 +0500

    begin

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
+import math
+from numba import njit
+import pygame as pg
+from config import FPS, TITLE
+import sys
+from screeninfo import get_monitors
+
+W = get_monitors()[0].width
+H = get_monitors()[0].height
+
+pg.init()
+flags = pg.FULLSCREEN
+window = pg.display.set_mode((W, H), flags, vsync=1)
+pg.display.set_caption(TITLE)
+clock = pg.time.Clock()
+background_menu = pg.image.load("img/background_menu.jpg")
+background_menu = pg.transform.scale(background_menu, (W, H))
+
+x, y = 0, 0
+time = 0
 
 
+def update_menu():
+    global window, x, y, time
+    time += 1
+    x = math.sin(time/100) * 100
+    y = math.cos(time/100) * 100
+    for i in pg.event.get():
+        if i.type == pg.KEYDOWN:
+            if i.key == pg.K_ESCAPE:
+                sys.exit()
 
 
-if __name__ == '__main__':
+def render_menu():
+    global x, y
+    window.blit(background_menu, (0, 0))
+    pg.draw.circle(window, (225, 225, 0), (300 + x, 300 + y), 30)
 
+
+def menu():
+
+    while True:
+        clock.tick(FPS)
+        update_menu()
+        render_menu()
+        pg.display.update()
+
+
+menu()
