1f240da718a7388de637789f65486c2652723b5f
FedorTrain <fedya_dryagin@mail.ru>
2022-01-19 12:37:30 +0500

    menu

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import math
 from numba import njit
+import math
 import pygame as pg
 from config import FPS, TITLE
 import sys
 from screeninfo import get_monitors
+import json
+from game import *
 
 W = get_monitors()[0].width
 H = get_monitors()[0].height
@@ -19,6 +21,43 @@ background_menu = pg.transform.scale(background_menu, (W, H))
 x, y = 0, 0
 time = 0
 
+entities = []
+backgrounds = []
+statics = []
+transports = []
+buttons = []
+
+
+def load_game(name):
+    global entities, backgrounds, statics, transports
+    with open("save/" + name + ".json", "r") as read_file:
+        loaded = json.load(read_file)
+    entities.clear()
+    for i in loaded['entities']:
+        entities.append(Entity(i['xy'][0], i['xy'][1], i["sprite"], i['speed'], i['backpack'],
+                               i['backpack_size'], i['job'], i['need']))
+    backgrounds.clear()
+    for i in loaded['backgrounds']:
+        backgrounds.append(Background(i['xy'][0], i['xy'][1], i["biome"]))
+    statics.clear()
+    for i in loaded['statics']:
+        statics.append(Static(i['xy'][0], i['xy'][1], i["sprite"]))
+    transports.clear()
+    for i in loaded['transports']:
+        transports.append(Transport(i['xy'][0], i['xy'][1], i["sprite"], i['tip']))
+
+
+def save_game(name):
+    global entities, backgrounds, statics, transports
+    data = {
+        "entities": [vars(item) for item in entities],
+        "backgrounds": [vars(item) for item in backgrounds],
+        "statics": [vars(item) for item in statics],
+        "transports": [vars(item) for item in transports]
+    }
+    with open("save/" + name + ".json", "w") as write_file:
+        json.dump(data, write_file, indent=2)
+
 
 def update_menu():
     global window, x, y, time
@@ -29,15 +68,34 @@ def update_menu():
         if i.type == pg.KEYDOWN:
             if i.key == pg.K_ESCAPE:
                 sys.exit()
+        if i.type == pg.MOUSEBUTTONDOWN:
+            for btn in buttons:
+                if i.pos[0] > btn.xy[0] and i.pos[1] > btn.xy[1]:
+                    if i.pos[0] < btn.xy[0] + btn.width and i.pos[1] < btn.xy[1] + btn.height:
+                        if btn.arg == 'non':
+                            btn.f()
+                        else:
+                            btn.f(btn.arg)
 
 
 def render_menu():
     global x, y
     window.blit(background_menu, (0, 0))
-    pg.draw.circle(window, (225, 225, 0), (300 + x, 300 + y), 30)
+    for btn in buttons:
+        window.blit(btn.img, btn.xy)
+    #pg.draw.circle(window, (225, 225, 0), (300 + x, 300 + y), 30)
 
 
 def menu():
+    buttons.clear()
+    img = pg.image.load("img/newgame.jpg")
+    tempbtn = Button(400, 200, 424, 76, img, "default")
+    tempbtn.f = load_game
+    buttons.append(tempbtn)
+    img = pg.image.load("img/exit.jpg")
+    tempbtn = Button(400, 500, 323, 147, img, "non")
+    tempbtn.f = sys.exit
+    buttons.append(tempbtn)
 
     while True:
         clock.tick(FPS)
@@ -46,4 +104,15 @@ def menu():
         pg.display.update()
 
 
+def test():
+    load_game("default")
+    save_game("default")
+    load_game("default")
+    save_game("default")
+    load_game("default")
+    save_game("default")
+    load_game("default")
+
 menu()
+
+
